a09c46ed90d6999afd29bbf133d1c1a1a406a546
Tran Luong Tien Si <91487292+tranluongtiensi@users.noreply.github.com>
2023-02-03 21:54:36 +0700

    connect app to firebase messaging and firestore

--- app/src/main/java/com/dop/chatapp/MainActivity.java
+++ app/src/main/java/com/dop/chatapp/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+//            if (!task.isSuccessful()) {
+//                Log.d("FCM", "Fetching FCM registration token failed", task.getException());
+//                return;
+//            }
+//
+//            // Get new FCM registration token
+//            String token = task.getResult();
+//            Log.d("FCM", "TOKEN:" + token);
+
+            // Log and toast
+//                String msg = getString(R.string.msg_token_fmt, token);
+//                Log.d("FCM", msg);
+//                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//        });
     }
 }
--- app/src/main/java/com/dop/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/dop/chatapp/activities/SignInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.dop.chatapp.R;
 import com.dop.chatapp.databinding.ActivitySigInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 
 public class SignInActivity extends AppCompatActivity {
@@ -24,5 +28,22 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
+    }
+
+    private void addDataToFirestore() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Tran");
+        data.put("last_name", "Si");
+        db.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data inserted", Toast.LENGTH_LONG).show();
+
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
